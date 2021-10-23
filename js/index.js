86518b0af0be13e4c1d8a59ba305c9dd002235ce
Aleksandr Leonchik <Aleksandr_Leonchik@epam.com>
2021-10-23 22:13:57 +0300

    dropbox file upload

--- js/index.js
+++ js/index.js
@@ -43,3 +43,35 @@ function addRow() {
 
 
 }
+
+function uploadFile(file) {
+    var xhr = new XMLHttpRequest();
+    file = file.files[0]
+    xhr.upload.onprogress = function (evt) {
+        var percentComplete = parseInt(100.0 * evt.loaded / evt.total);
+        // Upload in progress. Do something here with the percent complete.
+    };
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var fileInfo = JSON.parse(xhr.response);
+            // Upload succeeded. Do something here with the file info.
+        }
+        else {
+            var errorMessage = xhr.response || 'Unable to upload file';
+            // Upload failed. Do something here with the error.
+        }
+    };
+
+    xhr.open('POST', 'https://content.dropboxapi.com/2/files/upload');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + "");
+    xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+    xhr.setRequestHeader('Dropbox-API-Arg', JSON.stringify({
+        path: '/' + file.name,
+        mode: 'add',
+        autorename: true,
+        mute: false
+    }));
+
+    xhr.send(file);
+}
